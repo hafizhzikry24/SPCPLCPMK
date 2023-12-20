c1e7d066f09a2296d6bc1ba01241301c662e8b90
Ahmad Raditya <sayaiok2002@gmail.com>
2023-12-20 22:54:23 +0700

    ui update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,8 +1,9 @@
-<nav x-data="{ open: false }" class="bg-[#146C94] border-b border-gray-100 ">
+<nav x-data="{ open: false }" class="bg-[#0395FF] border-b border-gray-100 ">
     <!-- Primary Navigation Menu -->
 
-    <div class="flex justify-between h-16  bg-[#146C94] ">
+    <div class="flex justify-between h-16  bg-[#0395FF] ">
 
+<<<<<<< Updated upstream
             <div class="flex justify-between h-16 fixed top-0 bg-[#146C94] z-50 w-full">
                 <!-- Logo -->
                 <div class=" mx-12 shrink-0 flex items-center">
@@ -39,6 +40,42 @@
                         <x-slot name="content">
                             <x-dropdown-link :href="route('profile.edit')" >
                                 {{ __('Profile') }}
+=======
+        <div class="flex justify-between h-16 fixed top-0 bg-[#0395FF] z-50 w-full">
+            <!-- Logo -->
+            <div class="mx-12 shrink-0 flex items-center text-gray-100 text-xl font-bold">
+                SPCPLCPMK
+            </div>
+
+            <div class="hidden sm:flex sm:items-center sm:ml-8 justify mx-24 ">
+                <x-dropdown align="right" width="48">
+
+                    <x-slot name="trigger">
+                        <button class="inline-flex items-center px-3 py-1 border border-transparent text-sm leading-4 font-medium rounded-md text-black-800 bg-[#0395FF] hover:text-black-700 focus:outline-none transition ease-in-out duration-150">
+                            <x-academic-cap class="block h-4 w-auto fill-current text-gray-800" />
+                            <div>{{ Auth::user()->name }}</div>
+
+                            <div class="ml-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <x-dropdown-link :href="route('profile.edit')">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                {{ __('Log Out') }}
+>>>>>>> Stashed changes
                             </x-dropdown-link>
 
                             <!-- Authentication -->
@@ -63,35 +100,36 @@
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-1 border border-transparent text-sm leading-4 font-medium rounded-md text-black-800 bg-[#146C94] hover:text-black-700 focus:outline-none transition ease-in-out duration-150">
                             <x-academic-cap class="block h-4 w-auto fill-current text-gray-800" />
-                            <div>{{ Auth::user()->name }}</div>
+                            <div>{{ Auth::user()->name }}
+    </div>
 
-                            <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
+    <div class="ml-1">
+        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+        </svg>
+    </div>
+    </button>
+    </x-slot>
 
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')" >
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
+    <x-slot name="content">
+        <x-dropdown-link :href="route('profile.edit')">
+            {{ __('Profile') }}
+        </x-dropdown-link>
 
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
+        <!-- Authentication -->
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
 
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div> --}}
+                {{ __('Log Out') }}
+            </x-dropdown-link>
+        </form>
+    </x-slot>
+    </x-dropdown>
+    </div> --}}
 
+<<<<<<< Updated upstream
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
@@ -102,6 +140,18 @@
                 </button>
             </div>
         </div>
+=======
+    <!-- Hamburger -->
+    <div class="-mr-2 flex items-center sm:hidden">
+        <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </button>
+    </div>
+    </div>
+>>>>>>> Stashed changes
 
 
     <!-- Responsive Navigation Menu -->
@@ -128,8 +178,12 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
+<<<<<<< Updated upstream
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
+=======
+                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
+>>>>>>> Stashed changes
                                         this.closest('form').submit();" class="text-white">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
