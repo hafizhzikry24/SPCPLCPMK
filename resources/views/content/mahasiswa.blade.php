70b940e31f4326b8e542a67a9b6708682754f349
Ahmad Raditya <sayaiok2002@gmail.com>
2023-12-21 12:09:41 +0700

    feat: ui update

--- resources/views/content/mahasiswa.blade.php
+++ resources/views/content/mahasiswa.blade.php
@@ -40,11 +40,9 @@
                             </thead>
                         </table>
                         <!-- Div buat modal pop up -->
-                        <div class="py-12  bg-gray-100 bg-opacity-60 transition duration-150 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0"
-                            id="mahasiswa-modal">
+                        <div class="py-12  bg-gray-100 bg-opacity-60 transition duration-150 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0" id="mahasiswa-modal">
                             <div role="alert" class="container mx-auto w-11/12 md:w-2/3 max-w-lg">
-                                <div
-                                    class="relative mt-24 py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
+                                <div class="relative mt-24 py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
                                     <div class="w-full flex justify-start text-gray-600 mb-3">
                                     </div>
                                     <h1 class="text-gray-800 font-lg font-bold tracking-normal leading-tight mb-4">Tabel
@@ -74,27 +72,16 @@
                                     </head>
 
                                     <body>
-                                        <form action="javascript:void(0)" id="MahasiswaForm" name="MahasiswaForm"
-                                            class="form-horizontal" method="POST" enctype="multipart/form-data">
+                                        <form action="javascript:void(0)" id="MahasiswaForm" name="MahasiswaForm" class="form-horizontal" method="POST" enctype="multipart/form-data">
                                             <input type="hidden" name="id" id="id" />
-                                            <label for="nim"
-                                                class="text-gray-800 text-sm font-bold leading-tight tracking-normal">Nim</label>
-                                            <input name="nim" id="nim"
-                                                class="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-green-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
-                                                placeholder="21120120120034" />
-                                            <label for="nama"
-                                                class="text-gray-800 text-sm font-bold leading-tight tracking-normal">Nama</label>
-                                            <input name="nama" id="nama"
-                                                class="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-green-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
-                                                placeholder="Daffa" />
-                                            <label for="semester"
-                                                class="text-gray-800 text-sm font-bold leading-tight tracking-normal">Semester</label>
-                                            <input name="semester" id="semester"
-                                                class="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-green-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
-                                                placeholder="7" />
+                                            <label for="nim" class="text-gray-800 text-sm font-bold leading-tight tracking-normal">Nim</label>
+                                            <input name="nim" id="nim" class="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-green-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="21120120120034" />
+                                            <label for="nama" class="text-gray-800 text-sm font-bold leading-tight tracking-normal">Nama</label>
+                                            <input name="nama" id="nama" class="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-green-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Daffa" />
+                                            <label for="semester" class="text-gray-800 text-sm font-bold leading-tight tracking-normal">Semester</label>
+                                            <input name="semester" id="semester" class="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-green-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="7" />
                                             <div class="flex items-center justify-start w-full">
-                                                <button type="submit"
-                                                    class="focus:outline-none items-center
+                                                <button type="submit" class="focus:outline-none items-center
                                                     focus:ring-2
                                                     focus:ring-offset-2
                                                     focus:ring-green-700
@@ -106,22 +93,15 @@
                                                     text-sm mr-3">
                                                     Submit
                                                 </button>
-                                                <button type="button"
-                                                    class="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm"
-                                                    onclick="modalHandler(false)">Cancel</button>
+                                                <button type="button" class="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm" onclick="modalHandler(false)">Cancel</button>
                                         </form>
                                     </body>
 
                                     </html>
 
                                 </div>
-                                <button
-                                    class="cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out rounded focus:ring-2 focus:outline-none focus:ring-gray-600"
-                                    onclick="modalHandler()" aria-label="close modal" role="button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x"
-                                        width="20" height="20" viewBox="0 0 24 24" stroke-width="2.5"
-                                        stroke="currentColor" fill="none" stroke-linecap="round"
-                                        stroke-linejoin="round">
+                                <button class="cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out rounded focus:ring-2 focus:outline-none focus:ring-gray-600" onclick="modalHandler()" aria-label="close modal" role="button">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="20" height="20" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z" />
                                         <line x1="18" y1="6" x2="6" y2="18" />
                                         <line x1="6" y1="6" x2="18" y2="18" />
@@ -190,6 +170,10 @@
             });
 
             $('#mahasiswa').DataTable({
+                language: {
+                    search: '', // Mengosongkan teks pada kotak pencarian
+                    lengthMenu: '_MENU_', // Mengganti teks "Show Entries" dengan '_MENU_'
+                },
                 processing: true,
                 serverSide: true,
                 ajax: "{{ url('mahasiswa') }}",
