5020089635c153f8467b1ac5f56c69fd2e99be5d
Daffa Maulana Wisesa <daffamaulanawisesa@gmail.com>
2023-12-26 22:59:56 +0700

    Daffa-Page-Dosen
    Dosen page added and DosenTableSeeder

--- resources/views/content/dosen.blade.php
+++ resources/views/content/dosen.blade.php
@@ -1,4 +1,9 @@
 <x-app-layout>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.min.js"></script>
+    <link href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" rel="stylesheet">
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <div class="grid grid-cols-12">
         <!-- Sidebar -->
         <x-sidebar />
@@ -9,121 +14,116 @@
         <!-- Main Content -->
         <div class="col-span-10 overflow-y-auto mt-4">
             <main class="flex w-full justify-center h-screen pl-5 pr-5 pb-5">
-                <div class="w-full bg-white shadow-md rounded-md overflow-hidden border pl-5 pr-5 pt-5">
-                    <a class="text-3xl font-bold"> Tabel Data Dosen </a>
-                    <!-- Tombol Tambah -->
-                    <div class="p-5 flex justify-between items-center">
-                        <x-add-button type="submit" class="ml-auto" id="button">
-                            Tambah data
-                        </x-add-button>
-                    </div>
-                    <!-- Tabel Data -->
-                    <table class="min-w-full mt-4"> <!-- Ini daf kudu mbok garap Back End e sek nembe iso soal e styling e kudu ono Data Tables -->
-                        <thead class="border-b-2 border-black">
-                            <tr>
-                                <th class="  px-6 py-3 bg-[#F6F1F1] text-center text-xs leading-4 font-medium text-black uppercase tracking-wider">NIP</th>
-                                <th class=" px-6 py-3 bg-[#F6F1F1] text-center text-xs leading-4 font-medium text-black uppercase tracking-wider">Nama Dosen</th>
 
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <!-- Tampilkan data menggunakan loop -->
-                            {{-- @foreach() --}}
-                            <tr>
-                                <td class="px-6 py-4 whitespace-no-wrap"></td>
-                                <td class="px-6 py-4 whitespace-no-wrap"></td>
-                                <!-- Tambahkan kolom-kolom lain sesuai kebutuhan -->
-                                {{-- <td class="px-4 py-4 whitespace-no-wrap text-center">
-                                    <!-- Tombol Edit -->
-                                    <x-edit-button type="submit" id="button">
-                                        Edit
-                                    </x-edit-button>
-                                    <!-- Tombol Hapus -->
-                                    <form action="" method="POST" class="inline">
-                                        @csrf
-                                        @method('DELETE')
-                                        <x-danger-button type="submit">
-                                            Delete
-                                        </x-danger-button>
-                                    </form>
-                                </td> --}}
+                <body>
+                    <main class="flex w-full justify-center h-screen pl-5 pr-5 pb-5">
+                        <div class="w-full bg-white shadow-md rounded-md overflow-hidden border pl-5 pr-5 pt-5">
+                            <a class="text-3xl font-bold"> TABEL DOSEN </a>
+                            <div>
+                                <!-- Tabel Data -->
+                                <table class="table table-bordered" id="dosen">
+                                    <thead>
+                                        <tr>
+                                            <th class="bg-[#C2E7FF]" style=" border: none;">NIP Dosen</th>
+                                            <th class="bg-[#C2E7FF]" style=" border: none;">Nama Dosen</th>
+                                        </tr>
+                                    </thead>
+                                </table>
+                            </div>
+                            <!DOCTYPE html>
+                            <html>
 
-                            </tr>
-                            {{-- @endforeach --}}
-                        </tbody>
-                    </table>
-                </div>
-        </div>
-    </div>
-    </main>
+                            <head>
+                                <title>Data Dosen</title>
+                                <style>
+                                    table {
+                                        border-collapse: collapse;
+                                        width: 100%;
+                                    }
 
-    <div class="py-12  bg-gray-100 bg-opacity-60 transition duration-150 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0" id="modal">
-        <div role="alert" class="container mx-auto w-11/12 md:w-2/3 max-w-lg">
-            <div class="relative mt-24 py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
-                <div class="w-full flex justify-start text-gray-600 mb-3">
+                                    th,
+                                    td {
+                                        border-bottom: 2px solid #ddd;
+                                        padding: 8px;
+                                        text-align: left;
+                                    }
 
-                </div>
-                <h1 class="text-gray-800 font-lg font-bold tracking-normal leading-tight mb-4">Tabel CPL </h1>
-                <label for="cpl" class="text-gray-800 text-sm font-bold leading-tight tracking-normal">Nama CPL</label>
-                <input id="cpl" class="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-green-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="CPL-A" />
-                <label for="deskripsi" class="text-gray-800 text-sm font-bold leading-tight tracking-normal">Deskripsi</label>
-                <input id="deskripsi" class="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-green-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Lorem Ipsum" />
+                                    th {
+                                        background-color: #f2f2f2;
+                                    }
 
-                <div class="flex items-center justify-start w-full">
-                    <button class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-700 transition duration-150 ease-in-out hover:bg-green-300 bg-green-400 rounded text-white px-8 py-2 text-sm">Submit</button>
-                    <button class="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm" onclick="modalHandler()">Cancel</button>
-                </div>
-                <button class="cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out rounded focus:ring-2 focus:outline-none focus:ring-gray-600" onclick="modalHandler()" aria-label="close modal" role="button">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="20" height="20" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" />
-                        <line x1="18" y1="6" x2="6" y2="18" />
-                        <line x1="6" y1="6" x2="18" y2="18" />
-                    </svg>
-                </button>
-            </div>
+                                    .custom-search {
+                                        padding: 8px 30px 8px 40px;
+                                        /* Sesuaikan padding agar tulisan tidak terlalu dekat dengan tepi kotak */
+                                        border: 1px solid #ccc;
+                                        border-radius: 4px;
+                                        background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="%23757575" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><circle cx="11" cy="11" r="8" /><path d="M21 21l-6-6" /></svg>');
+                                        background-repeat: no-repeat;
+                                        background-position: calc(100% - 20px) center;
+                                        /* Mulai dari tepi kanan */
+                                        background-size: 20px;
+                                        /* Ukuran ikon */
+                                        padding-right: 40px;
+                                        /* Menggeser teks untuk memberi ruang bagi ikon */
+                                        width: 200px;
+                                        /* Sesuaikan lebar sesuai kebutuhan */
+                                        color: #757575;
+                                        /* Warna teks abu-abu */
+                                        transition: background-position 0.3s ease-in-out;
+                                        /* Efek transisi saat fokus */
+                                    }
+
+                                    .dataTables_filter {
+                                        text-align: right;
+                                        /* Posisi kotak pencarian di sisi kanan */
+                                        margin-bottom: 10px;
+                                        /* Jarak antara kotak pencarian dan tabel */
+                                    }
+                                </style>
+                            </head>
+                    </main>
         </div>
     </div>
 
-    <script>
-        let modal = document.getElementById("modal");
 
-        function modalHandler(val) {
-            if (val) {
-                fadeIn(modal);
-            } else {
-                fadeOut(modal);
-            }
-        }
-
-        function fadeOut(el) {
-            el.style.opacity = 1;
-            (function fade() {
-                if ((el.style.opacity -= 0.1) < 0) {
-                    el.style.display = "none";
-                } else {
-                    requestAnimationFrame(fade);
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 }
-            })();
-        }
+            });
 
-        function fadeIn(el, display) {
-            el.style.opacity = 0;
-            el.style.display = display || "flex";
-            (function fade() {
-                let val = parseFloat(el.style.opacity);
-                if (!((val += 0.2) > 1)) {
-                    el.style.opacity = val;
-                    requestAnimationFrame(fade);
-                }
-            })();
-        }
-
-        // Hide the modal when the page is first rendered
-        modal.style.display = "none";
-
-        // Tambahkan pemanggilan modalHandler(false) pada saat halaman ini dimuat
-        document.addEventListener("DOMContentLoaded", function() {
-            modalHandler(false);
+            $('#dosen').DataTable({
+                language: {
+                    search: '', // Mengosongkan teks pada kotak pencarian
+                    lengthMenu: '_MENU_', // Mengganti teks "Show Entries" dengan '_MENU_'
+                },
+                processing: true,
+                serverSide: true,
+                ajax: "{{ url('dosen') }}",
+                columns: [{
+                        data: 'NIP',
+                        name: 'NIP'
+                    },
+                    {
+                        data: 'Nama_Dosen',
+                        name: 'Nama_Dosen'
+                    },
+                ],
+                order: [
+                    [0, 'desc']
+                ],
+                // Customizing the DataTables elements position
+                dom: '<"flex mb-3 mt-3"l<"flex-shrink-0 mr-3 ml-3"f>>rtip',
+                initComplete: function() {
+                    // Menyesuaikan kotak pencarian
+                    $('.dataTables_filter input[type="search"]').addClass('custom-search');
+                },
+                lengthMenu: [10, 20, 30],
+                pageLength: 10
+            });
+            $('.dataTables_length select').addClass('px-2 py-1 w-16 rounded');
         });
     </script>
 </x-app-layout>
