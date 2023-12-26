5057cf88b94399f22b64e40576d8bc38535b3687
hafizhzikry24 <81897441+hafizhzikry24@users.noreply.github.com>
2023-12-26 15:47:13 +0700

    feat:update ui

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,19 +15,19 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center px-12 py-40 sm:pt-0" style="background-image: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('{{ asset("build/assets/background.jpg") }}'); background-size: cover; background-position: center center;">
-            <div>
+        <div class="min-h-screen flex flex-col sm:justify-center items-center px-12 py-40 sm:pt-0" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('{{ asset("build/assets/background.jpg") }}'); background-size: cover; background-position: center center;">
+
                 {{-- <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a> --}}
-            </div>
+
 
             {{-- <div class="w-full sm:max-w-md px-6 py-4 bg-blue-500 shadow-md overflow-hidden sm:rounded-lg">
                 <a class="text-black text-3xl font-bold text-center flex items-center justify-center">SPCPLCPMK</a>
             </div> --}}
 
-            <div class="w-full sm:max-w-md mt-4 px-6 py-12 bg-[#146C94] bg-opacity-70 shadow-md overflow-hidden sm:rounded-lg backdrop-filter backdrop-blur-md border-2 border-gray-600">
-                <a class="text-white text-3xl mb-4 font-bold text-center flex items-center justify-center border-b-2 border-black">SPCPLCPMK</a>
+            <div class="w-full sm:max-w-md mt-24 px-1 py-16 bg-[#004D85CC] bg-opacity-100 shadow-md overflow-hidden sm:rounded-3xl backdrop-filter backdrop-blur-md ">
+                <a class="text-white text-3xl mb-4 font-bold text-center flex justify-center ">SPCPLCPMK</a>
 
                 {{ $slot }}
             </div>
