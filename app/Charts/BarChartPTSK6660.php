48c740c2f1642b164fce5bd43f5b635665fee2ff
Daffa Maulana Wisesa <daffamaulanawisesa@gmail.com>
2024-02-05 10:53:15 +0700

    Daffa-Feature-CPL Graph PTSK6660

--- app/Charts/BarChartPTSK6660.php
+++ app/Charts/BarChartPTSK6660.php
@@ -16,10 +16,32 @@ class BarChartPTSK6660
     public function build(): \ArielMejiaDev\LarapexCharts\BarChart
     {
         return $this->BarChart->barChart()
-            ->setTitle('San Francisco vs Boston.')
-            ->setSubtitle('Wins during season 2021.')
-            ->addData('San Francisco', [6, 9, 3, 4, 10, 8])
-            ->addData('Boston', [7, 3, 8, 2, 6, 4])
-            ->setXAxis(['January', 'February', 'March', 'April', 'May', 'June']);
+            ->setTitle('Grafik CPL')
+            ->setSubtitle('Perbandingan Nilai CPL')
+            ->addData('4', [
+                \App\Models\PTSK6660::where('cpl2', 4)->count(),
+                \App\Models\PTSK6660::where('cpl3', 4)->count(),
+                \App\Models\PTSK6660::where('cpl4', 4)->count(),
+                \App\Models\PTSK6660::where('cpl6', 4)->count(),
+            ])
+            ->addData('3', [
+                \App\Models\PTSK6660::where('cpl2', 3)->count(),
+                \App\Models\PTSK6660::where('cpl3', 3)->count(),
+                \App\Models\PTSK6660::where('cpl4', 3)->count(),
+                \App\Models\PTSK6660::where('cpl6', 3)->count(),
+            ])
+            ->addData('2', [
+                \App\Models\PTSK6660::where('cpl2', 2)->count(),
+                \App\Models\PTSK6660::where('cpl3', 2)->count(),
+                \App\Models\PTSK6660::where('cpl4', 2)->count(),
+                \App\Models\PTSK6660::where('cpl6', 2)->count(),
+            ])
+            ->addData('1', [
+                \App\Models\PTSK6660::where('cpl2', 1)->count(),
+                \App\Models\PTSK6660::where('cpl3', 1)->count(),
+                \App\Models\PTSK6660::where('cpl4', 1)->count(),
+                \App\Models\PTSK6660::where('cpl6', 1)->count(),
+            ])
+            ->setXAxis(['CPL 2', 'CPL 3', 'CPL 4', 'CPL 6']);
     }
 }
