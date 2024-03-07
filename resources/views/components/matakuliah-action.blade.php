abdb7d2561b6388a08aaea3abd569105370a9317
Daffa Maulana Wisesa <daffamaulanawisesa@gmail.com>
2024-03-08 06:01:33 +0700

    Daffa-Update-Admin Page

--- resources/views/components/matakuliah-action.blade.php
+++ resources/views/components/matakuliah-action.blade.php
@@ -1,24 +1,22 @@
-<x-edit-button>
-    <a href="javascript:void(0)" data-toggle="tooltip" onClick='editFunc({{ $id }})'' data-original-title="Edit"
-        class="edit btn btn-success edit">
+<x-edit-button onClick='editFunc({{ $id }})'>
+    <a href="javascript:void(0)" data-toggle="tooltip" data-original-title="Edit" class="edit btn btn-success edit">
         Edit
     </a>
 </x-edit-button>
 
 @if ($isAdmin)
     <!-- Show admin-specific content or buttons -->
-    <x-danger-button>
-        <a href="javascript:void(0);" id="delete-company" onClick='deleteFunc({{ $id }})' data-toggle="tooltip"
-            data-original-title="Delete" class="delete btn btn-danger">
+    <x-danger-button onClick='deleteFunc({{ $id }})'>
+        <a href="javascript:void(0);" id="delete-company" data-toggle="tooltip" data-original-title="Delete"
+            class="delete btn btn-danger">
             Remove
         </a>
     </x-danger-button>
 @endif
 
-<x-nilai-button>
-    <a type="button"
-        href="{{ route('mata_kuliah', ['tahun_akademik_matkul' => $tahun_akademik, 'semester_matkul' => $semester, 'matkul_id' => $kode_MK]) }}"
-        data-toggle="tooltip" data-original-title="View Details" class="btn btn-info">
+<x-nilai-button
+    onclick="window.location.href='{{ route('mata_kuliah', ['tahun_akademik_matkul' => $tahun_akademik, 'semester_matkul' => $semester, 'matkul_id' => $kode_MK]) }}'">
+    <a type="button" data-toggle="tooltip" data-original-title="View Details" class="btn btn-info">
         Nilai
     </a>
 </x-nilai-button>
