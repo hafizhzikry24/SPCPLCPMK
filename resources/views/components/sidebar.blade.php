da281938a33a852a545ead2e4d99562c5af962d4
hafizhzikry24 <81897441+hafizhzikry24@users.noreply.github.com>
2024-06-14 21:39:12 +0700

    feat:update

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -51,7 +51,7 @@
         @if (auth()->user()->isAdmin())
             <li class="my-8 mx-3 sm:rounded-xl hover:bg-[#DFF3FF] @if (Request::route()->getName() === 'admin') bg-[#DFF3FF] @endif">
                 <a href="{{ route('admin') }}" class="my-3 mx-3 inline-flex items-center text-gray-500 w-full">
-                    <x-assets.user class="w-4 h-4 mr-1" />
+                    <x-assets.history class="w-4 h-4 mr-1" />
                     <span class="ml-2 text-1xl">History</span>
                 </a>
             </li>
